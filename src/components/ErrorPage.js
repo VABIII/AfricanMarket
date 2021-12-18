b2c16b4befc1120de95c8f3e4e823e5752bb2c3b
VABIII <tbengtson82@gmail.com>
2021-12-18 01:46:15 -0600

    connected Routes

---
+++ src/components/ErrorPage.js
@@ -0,0 +1,60 @@
+import React from 'react';
+
+const ErrorPage = () => {
+    return (
+        <div>
+            <h1>Error: Page does not exist</h1>
+        </div>
+    );
+};
+
+export default ErrorPage;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/components/Footer.js
@@ -0,0 +1,60 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div>
+            <h1>Footer</h1>
+        </div>
+    );
+};
+
+export default Footer;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
