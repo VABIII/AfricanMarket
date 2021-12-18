9c9ee94dbe52d7057519bfd73f2ce98ef2bbce8d
VABIII <tbengtson82@gmail.com>
2021-12-17 23:18:50 -0600

    added components and dummy data

---
+++ src/components/ProductInfo.js
@@ -0,0 +1,60 @@
+import React from 'react';
+
+const ProductInfo = () => {
+    return (
+        <div>
+            <h1>Product Info</h1>
+        </div>
+    );
+};
+
+export default ProductInfo;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/initialValues/initialValues.js
@@ -0,0 +1,168 @@
+export const productArray = [
+    {
+        productName: "Banana",
+        productPrice: "$10",
+        location: 'North Africa',
+        productDescription: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod" +
+            " tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud" +
+            " exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor" +
+            " in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint" +
+            " occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    },
+
+    {
+        productName: "Banana",
+        productPrice: "$10",
+        location: 'South Africa',
+        productDescription: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod" +
+            " tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud" +
+            " exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor" +
+            " in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint" +
+            " occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    },
+
+    {
+        productName: "Banana",
+        productPrice: "$10",
+        location: 'Central Africa',
+        productDescription: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod" +
+            " tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud" +
+            " exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor" +
+            " in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint" +
+            " occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    },
+
+    {
+        productName: "Banana",
+        productPrice: "$10",
+        location: 'South Africa',
+        productDescription: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod" +
+            " tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud" +
+            " exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor" +
+            " in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint" +
+            " occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    },
+
+    {
+        productName: "Banana",
+        productPrice: "$10",
+        location: 'Central Africa',
+        productDescription: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod" +
+            " tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud" +
+            " exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor" +
+            " in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint" +
+            " occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    },
+
+    {
+        productName: "Banana",
+        productPrice: "$10",
+        location: 'North Africa',
+        productDescription: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod" +
+            " tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud" +
+            " exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor" +
+            " in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint" +
+            " occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    },
+
+    {
+        productName: "Banana",
+        productPrice: "$10",
+        location: 'Central Africa',
+        productDescription: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod" +
+            " tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud" +
+            " exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor" +
+            " in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint" +
+            " occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    },
+
+    {
+        productName: "Banana",
+        productPrice: "$10",
+        location: 'South Africa',
+        productDescription: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod" +
+            " tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud" +
+            " exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor" +
+            " in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint" +
+            " occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    },
+
+    {
+        productName: "Banana",
+        productPrice: "$10",
+        location: 'Central Africa',
+        productDescription: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod" +
+            " tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud" +
+            " exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor" +
+            " in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint" +
+            " occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    },
+];
+
+export const signupInitialValues = {
+    username: "",
+    firstName: "",
+    lastName: "",
+    businessName:"",
+    jobTitle: "",
+    avatarImg: "",
+    password: ""
+};
+
+export const newProductInitialValues = {
+    productName: "",
+    productDescription: "",
+    productPrice: "",
+    location: ""
+};
+
+export const loginInitialValues = {
+    username: "",
+    password: ""
+};
+
+export const newUser = {
+    username: "Logan123",
+    firstName: "James",
+    lastName: "Howlett",
+    businessName: "Patches, Inc",
+    jobTitle: "Chief of Operations",
+    avatarImg: ""
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
