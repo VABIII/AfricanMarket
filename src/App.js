b65316177dcf90c571a4e18414101ca3537281ce
VABIII <tbengtson82@gmail.com>
2021-12-17 20:07:37 -0600

    imported components into App file

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from "react";
+import {Route} from "react-router-dom";
 import './App.css';
+import Header from "./components/Header";
+import Login from "./components/Login";
+import Logout from "./components/Logout";
+import Home from "./components/Home";
+import Product from "./components/Product";
+import Products from "./components/Products";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return(
+      <div className="app-container">
+        <h1>App</h1>
+        <Home/>
+        <Logout/>
+        <Login/>
+        <Product/>
+        <Products/>
+      </div>
+  )
 }
 
 export default App;
