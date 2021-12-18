0db5f45d87d15ef472de60928f93feace16fcf60
VABIII <tbengtson82@gmail.com>
2021-12-18 00:46:20 -0600

    routes setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React, {useState, useEffect} from "react";
-import {Route} from "react-router-dom";
+import {Route, Routes} from "react-router-dom";
 import './App.css';
 import Header from "./components/Header";
 import Login from "./components/Login";
 import Logout from "./components/Logout";
 import Home from "./components/Home";
-import Product from "./components/Product";
 import Products from "./components/Products";
 import SignUp from "./components/SignUp";
 
@@ -14,13 +13,15 @@ function App() {
 
   return(
       <div className="app-container">
-        <h1>App</h1>
-        <Home/>
-        <Logout/>
-        <Login/>
-        <Products/>
-        <Product/>
-        <SignUp/>
+        <h1>African Market</h1>
+        <Header/>
+        <Routes>
+          <Route path="/logout" element={<Logout/>}/>
+          <Route path="/login" element={<Login/>}/>
+          <Route path="/products" element={<Products/>}/>
+          <Route path="/signup" element={<SignUp/>}/>
+          <Route path="/" element={<Home/>}/>
+        </Routes>
       </div>
   )
 }
