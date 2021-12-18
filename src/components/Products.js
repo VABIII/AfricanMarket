c1c545da870bb4ad9f296101781f5b32071b181a
VABIII <tbengtson82@gmail.com>
2021-12-18 03:45:19 -0600

    Built out and some styling on Products, Product, & ProductInfo components

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,9 +1,38 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from "axios";
+import { productArray } from "../initialValues/initialValues";
+import Product from "./Product";
+import "../styles/Products.css"
+
 
 const Products = () => {
+    const [products, setProducts] = useState(productArray);
+
+
+    // useEffect(() => {
+    //     axios.get("")
+    //         .then(res => {
+    //             setProducts(res.data);
+    //         })
+    //         .catch(err => {
+    //             console.error(err);
+    //         })
+    //
+    // }, []);
+
     return (
-        <div>
-            <h1>Products</h1>
+        <div className="products-container">
+            <div className="products-title">
+                <strong><h1>Products</h1></strong>
+            </div>
+            <div className="products">
+                {
+                    products.map((product, i) => {
+                        return <Product key={i} product={product}/>
+                    })
+                }
+            </div>
+
         </div>
     );
 };
