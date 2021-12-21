68ae9dc0a4ef3923d5865b5bd995faad604e4fe6
Ruth Muiruri <rmuiruri911@gmail.com>
2021-12-21 16:48:25 -0600

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,56 +1,28 @@
 import React from 'react';
-
+import {FooterContainer, FooterLink, FooterLinkItems, FooterLinkTitle, FooterLinksContainer, FooterLinksWrapper, FooterWrap, SocialLogo, WebsiteRights} from './FooterElements';
 const Footer = () => {
     return (
-        <div>
-            <h1>Footer</h1>
-        </div>
+        <FooterContainer>
+            <FooterWrap>
+                <FooterLinksContainer>
+                    <FooterLinksWrapper>
+                        <FooterLinkItems>
+                            <FooterLinkTitle> About</FooterLinkTitle>
+                            <FooterLink to = '/login'> Login</FooterLink>
+                            <FooterLink to = '/login'> User Profile </FooterLink>
+                            <FooterLink to = '/login'> Products</FooterLink>
+                            <FooterLink to = '/'> Contact Us</FooterLink>
+                        </FooterLinkItems>
+                    </FooterLinksWrapper>
+                </FooterLinksContainer>
+                    <SocialLogo to= '/'> African MarketPlace</SocialLogo>
+                    <WebsiteRights> African MarketPlace © {new Date().getFullYear()}</WebsiteRights>                
+            </FooterWrap>
+        </FooterContainer>
     );
 };
 
 export default Footer;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
