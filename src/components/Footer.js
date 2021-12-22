c19fab5d90f4d3525e9752f56e46a7e0e6368f6a
Ruth Muiruri <rmuiruri911@gmail.com>
2021-12-22 17:22:06 -0600

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,34 +1,25 @@
 import React from 'react';
-import {FooterContainer, FooterLink, FooterLinkItems, FooterLinksContainer,
-    FooterLinksWrapper, FooterWrap, SocialLogo, WebsiteRights} from './FooterElements';
-
-
+import {FooterContainer, FooterLink, FooterLinkItems, FooterLinkTitle, FooterLinksContainer, FooterLinksWrapper, FooterWrap, SocialLogo, WebsiteRights} from './FooterElements';
 const Footer = () => {
     return (
         <FooterContainer>
             <FooterWrap>
-                <SocialLogo to= '/'> African MarketPlace</SocialLogo>
-                <WebsiteRights> African MarketPlace © {new Date().getFullYear()}</WebsiteRights>
                 <FooterLinksContainer>
                     <FooterLinksWrapper>
                         <FooterLinkItems>
+                            <FooterLinkTitle> About</FooterLinkTitle>
                             <FooterLink to = '/login'> Login</FooterLink>
-                            <FooterLink to = '/user'> User Profile </FooterLink>
-                            <FooterLink to = '/products'> Products</FooterLink>
-                            <FooterLink to = '/about'>About</FooterLink>
+                            <FooterLink to = '/login'> User Profile </FooterLink>
+                            <FooterLink to = '/login'> Products</FooterLink>
+                            <FooterLink to = '/ContactUs'> Contact Us</FooterLink>
                         </FooterLinkItems>
                     </FooterLinksWrapper>
                 </FooterLinksContainer>
+                    <SocialLogo to= '/'> African MarketPlace</SocialLogo>
+                    <WebsiteRights> African MarketPlace © {new Date().getFullYear()}</WebsiteRights>                
             </FooterWrap>
         </FooterContainer>
     );
 };
 
 export default Footer;
-
-
-
-
-
-
-
