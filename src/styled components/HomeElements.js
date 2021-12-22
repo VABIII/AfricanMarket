e24fc5527106087538d5dff28e992ec1498b2496
VABIII <tbengtson82@gmail.com>
2021-12-21 23:34:40 -0600

    home styled comps

---
+++ src/styled components/HomeElements.js
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+
+export const TitlePage = styled.h1`
+  text-align: center;
+  margin-top: 3%;
+`;
+export const MapImg = styled.img`
+  width: 100%;
+  height: 500px;
+  object-fit: cover;
+  object-position: 10% 75%;
+`;
+export const InfoStuff = styled.div`
+  text-align: center;
+  display: flex;
+  flex-direction: column;
+  border-style: solid;
+  background-color: #5DB0F1; 
+`;
+export const InfoStuff2 = styled.div`
+border-style: solid;
+margin: 5% 20%;
+padding: 0 20%;
+background-color: #AC9A1F;
+`;
